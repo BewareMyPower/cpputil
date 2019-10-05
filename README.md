db37158fabe743fdc19b52a096a40d4596a961a6
BewareMyPower <xyzinfernity@163.com>
2019-10-05 04:28:05 -0700

    Simplify build script

--- README.md
+++ README.md
@@ -4,35 +4,23 @@ A Linux util library based on C++11.
 
 ## How to use
 
-This project also show a simple usage of cmake, the struct of this project is:
+Install **cpputil** to `<INSTALL_PATH>`:
 
-```
-$ tree . -L 1
-.
-├── build.sh
-├── CMakeLists.txt
-├── tests            // Unit tests which use cpputil library.
-├── README.md
-└── src              // Sources of cpputil library.
+```bash
+./build.sh -i <INSTALL_PATH>
 ```
 
-You can build the library simply by run `./build.sh`, and following directories will be added:
+If you want to build test binaries, add options `--with-tests`.
 
-```
-bin     // Installed executable binaries.
-include // Installed header files. 
-lib     // Installed libraries. For now, it's just *libcpputil.a* .
-```
+Uninstall **cpputil** under `<DIR>`:
 
-You can modify `CMAKE_INSTALL_PREFIX` in [build.sh](./build.sh) to change **INSTALL_PATH**, it's default in current directory.
-
-After that, you can imitate [tests/CMakeLists.txt](./tests/CMakeLists.txt) to write your own CMakeLists.txt to use cpputil, or simply write a Makefile. If your project has only one file (eg. `myproject.cc`), use g++ directly, for example:
-
-```
-g++ myproject.cc -std=c++11 -I <INSTALL_PATH>/include -L <INSTALL_PATH>/lib/libcpputil.a
+```bash
+./build.sh -u <DIR>
 ```
 
-Run `./build.sh clean` to remove `build` directory, `./build uninstall` to remove `include`, `lib`, `bin`.
+Run `./build.sh -h` to see more details.
+
+See examples in [tests](tests/) or installed headers to learn how to use.
 
 ## TODO
 
--- build.sh
+++ build.sh
@@ -1,28 +1,108 @@
 #!/bin/bash
-SHELL_DIR=$(cd $(dirname $0) && pwd)
-INSTALL_DIR=$SHELL_DIR
-cd $SHELL_DIR
+set -o errexit
 
-if [ $# -eq 1 ]; then
-    case $1 in
-        "clean")
-            echo "rm -rf $SHELL_DIR/build"
-            rm -rf $SHELL_DIR/build
-            exit 0
-            ;;
-        "uninstall")
-            echo "rm -rf $INSTALL_DIR/bin $INSTALL_DIR/lib $INSTALL_DIR/include"
-            rm -rf $INSTALL_DIR/bin $INSTALL_DIR/lib $INSTALL_DIR/include
-            exit 0
-            ;;
-        *)
-            echo "Invalid command: "$1
-            exit 1
-            ;;
+function usage() {
+    printf "\
+Usage:
+  %s [OPTIONS]
+
+Options:
+  -i, --install DIR   install headers, libraries and binaries to DIR, following
+                      subdirectories will be created:
+                        include/  Headers (*.h)
+                        lib/      Libraries (*.a)
+                        bin/      Unit test binaries if --with-tests was specified
+                      (default: /usr/local)
+  -u, --uninstall DIR remove headers, libraries and binaries under DIR
+                      other options will be ignored if it's specified
+  --with-tests        build unit test binaries
+  -h, --help          display this help and exit\n" \
+      $1
+}
+
+function check_dir() {
+    if [[ -e $1 ]] && [[ ! -d $1 ]]; then
+        echo "$1 exists but it's not a directory"
+        exit 1
+    fi
+}
+
+WITH_TESTS=false
+
+function install() {
+    check_dir $1
+    mkdir -p $1
+    INSTALL_DIR=`cd $1 && echo $PWD`
+    CMAKE_OPTIONS="-DCMAKE_INSTALL_PREFIX=$INSTALL_DIR"
+    if [[ $WITH_TESTS == true ]]; then
+        CMAKE_OPTIONS="$CMAKE_OPTIONS -DCMAKE_BUILD_TESTS=1"
+    fi
+    mkdir -p build && cd build
+    echo "Install cpputil to $INSTALL_DIR"
+    cmake .. $CMAKE_OPTIONS
+    make
+    make install
+}
+
+function uninstall() {
+    check_dir $1
+    UNINSTALL_DIR=${1%/}
+    echo $UNINSTALL_DIR
+    SUBDIRS=(bin lib include)
+    echo "Following directories will be removed:"
+    for SUBDIR in ${SUBDIRS[@]}; do
+        echo "  $UNINSTALL_DIR/$SUBDIR"
+    done
+
+    while true; do
+        echo -n "Are you sure? [y/N]: "
+        read ANSWER
+        if [[ $ANSWER =~ ^[yYnN] ]]; then
+            if [[ ${ANSWER:0:1} = [yY] ]]; then
+                for SUBDIR in ${SUBDIRS[@]}; do
+                    COMMAND="rm -rf $UNINSTALL_DIR/$SUBDIR"
+                    echo $COMMAND && eval $COMMAND
+                done
+            fi
+            break
+        fi
+    done
+}
+
+# parse args
+OPTIONS=hi:u:
+LONGOPTS=help,install:,uninstall:,with-tests
+! PARSED=$(getopt --options=$OPTIONS --longoptions=$LONGOPTS --name "$0" -- "$@")
+if [[ ${PIPESTATUS[0]} -ne 0 ]]; then
+    exit 2
+fi
+eval set -- "$PARSED"
+
+UNINSTALL=false
+CMAKE_INSTALL_PREFIX=/usr/local
+
+while true; do
+    case "$1" in
+        -h|--help) usage $0; exit 0; ;;
+        -i|--install) CMAKE_INSTALL_PREFIX=$2; shift 2; ;;
+        -u|--uninstall) UNINSTALL=true; UNINSTALL_DIR=$2; shift 2; ;;
+        --with-tests) WITH_TESTS=true; shift; ;;
+        --) shift; break; ;;
+        *) echo "Programming error"; exit 3; ;;
     esac
+done
+
+if [[ $# -ne 0 ]]; then
+    echo "$0: no extra command-line arguments were needed except options"
+    usage $0
+    exit 4
 fi
 
-mkdir -p build
-cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR && make && make install
-cd ..
+if [[ $UNINSTALL == false ]]; then
+    SHELL_DIR=`cd $(dirname $0) && echo $PWD`
+    cd $SHELL_DIR
+    install $CMAKE_INSTALL_PREFIX
+else
+    uninstall $UNINSTALL_DIR
+    exit 0
+fi
